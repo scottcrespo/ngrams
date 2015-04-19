08d7e6cd473968287f2f4a7564c610ea830cefd7
Scott Crespo <screspo@highcornergroup.com>
2015-04-19 10:26:42 -0400

    work in progress

--- Trigram.java
+++ Trigram.java
@@ -11,8 +11,31 @@ public class Trigram implements WritableComparable {
     private Text second;
     private Text third;
     
-    public void write(DataOutput out) throws IOException {
+    public Trigram(Text first, Text second, Text third) {
+        /* constructor takes three Text objects */
+        set(first,second,third);
+    }
+    
+    public Trigram() {
+        /* 
+        Instantiation without params creates a trigram with
+        three empty Text objects 
+        */
+        Text empty = new Text();
+        set(empty, empty, empty);
+    }
+    
+    public void set(Text first, Text second, Text third) {
         
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+    
+    public void write(DataOutput out) throws IOException {
+        first.write(out);
+        second.write(out);
+        third.write(out);
     }
     
     public void readFields(DataInput in) throws IOException {
