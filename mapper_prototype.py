b1bae92f4d469d9fdeac5e951e10e91d19035a93
Scott Crespo <screspo@highcornergroup.com>
2015-04-20 10:13:49 -0400

    more debugging output

--- mapper_prototype.py
+++ mapper_prototype.py
@@ -6,30 +6,33 @@ def test_mapper():
     a simple string
     """
     
-    line = "the quick brown fox jumped over the lazy dog"
-    words = line.split()
+    lines = ["the quick brown fox jumped over the lazy dog","the quick brown","the quick"]
+    
+    for line in lines:
+        
+        words = line.split()
+        length = len(words)
+        sys.stdout.write("\nLength of %d \n-------------------\n" % length)
 
-    length = len(words)
+        for i, word in enumerate(words, 0):
 
-    for i, word in enumerate(words, 0):
+            if i <= 1:
+                first = "#"
+            else: first = words[i]
 
-        if i <= 1:
-            first = "#"
-        else: first = words[i]
+            if i==0 or i+1==length:
+                second = "#"
+            else: second = words[i+1]
 
-        if i==0 or i+1==length:
-            second = "#"
-        else: second = words[i+1]
+            if i+2==length or i+1 ==length:
+                third = "#"
+            else: third = words[i+2]
 
-        if i+2==length or i+1 ==length:
-            third = "#"
-        else: third = words[i+2]
+            i += 1
 
-        i += 1
-
-        trigram = "%s %s %s \n" % (first, second, third)
-
-        sys.stdout.write(trigram)
+            trigram = "%s %s %s \n" % (first, second, third)
+            
+            sys.stdout.write(trigram)
         
 if __name__ == "__main__":
     test_mapper()
